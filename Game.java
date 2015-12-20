5b93e5afb3b09ef67eb1ba2b26f29797f2c07e2e
which0326 <which0326@gmail.com>
2015-12-20 23:41:41 +0800

    add 2 version, term and UI

--- Game.java
+++ Game.java
@@ -1,26 +1,26 @@
+
 import java.io.*;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class Game{
 	static final int camp_num = 2;
 	static final int total_player_num = Constants.player_num * camp_num;
 	public static Player[] player = new Player[ total_player_num ];
-
+	public static Tower[] tower = new Tower[ camp_num ];
+	public static int round;
+	public static int turn ;
+	public static boolean blockmain;
+	
 	public static void main(String[] args) throws IOException{
 		// initializstion
 		int i,j;
-		int round=1;
 		int one_side_player_num = Constants.player_num;
 
-		Tower[] tower = new Tower[ camp_num ];
 		Computer computer = new Computer();
 		ActivityMenu AM = new ActivityMenu();
-		//	new a Frame
-		MainFrame window = new MainFrame();
-		window.frame.setVisible(true);
-
-
-
+		round = 1;
+		turn = 1;
 
 		for(i=0; i<camp_num; i++){
 			tower [ i ] = new Tower( i+1 );
@@ -36,33 +36,122 @@ public class Game{
 		// public static Player player1 = new Player(1, 1);
 		// player1 = player[0];
 
-		System.out.println( "################## Two Camp Game Start!! ##################");
-		print_status( player );
+		int term_or_UI = Integer.parseInt(args[0]);
 
-		while(round<3){
-			System.out.println( "###################### Round <"+round+"> ######################");
+		if( term_or_UI == 0){ // for terminal usage
 
-			// initialize the soldier number
-			for(i=0; i<camp_num; i++){
-				tower[i].setDefSoldier(0); 
-				tower[i].setOffSoldier(0);
-			}
-
-			for(i=0; i<player.length; i++){
-				player[i].setNumDefSoldier(0);
-				player[i].setNumOffSoldier(0);
-			}
-
-			take_action(AM, player, tower); 
-
-			computer.fight(tower[0], tower[1], player);  // assume 2 camp
-
-			System.out.println( "#########################################################");
+			System.out.println( "################## Two Camp Game Start!! ##################");
 			print_status( player );
 
-			round++;
+			while(round<3){
+				System.out.println( "###################### Round <"+round+"> ######################");
+
+				// initialize the soldier number
+				for(i=0; i<camp_num; i++){
+					tower[i].setDefSoldier(0); 
+					tower[i].setOffSoldier(0);
+				}
+
+				for(i=0; i<player.length; i++){
+					player[i].setNumDefSoldier(0);
+					player[i].setNumOffSoldier(0);
+				}
+
+				take_action(AM, player, tower); 
+
+				computer.fight(tower[0], tower[1], player);  // assume 2 camp
+
+				System.out.println( "#########################################################");
+				print_status( player );
+
+				round++;
+			}
+		}else if(term_or_UI == 1){ // for GUI usage
+			MainFrame window = new MainFrame();
+			//	new a Frame
+			window.frame.setVisible(true);
+			blockmain = true;
+
+			while( blockmain ){
+				// do nothing
+				try {
+				    Thread.sleep(200);                 //1000 milliseconds is one second.
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+
+				// System.out.println("waiting game start");
+			}
+
+			MainFrame.textArea.append("############ Two Camp Game Start!! ############\n\n");
+			print_status_to_UI( player, MainFrame.textArea);
+
+			// true game loop 
+			while(round <= Constants.total_round){
+				// set player1 action
+				blockmain = true;
+				///// player 1
+				// action
+				print_msg_to_UI("\n############ round " + round + " ############", MainFrame.textArea);
+				MainFrame.actionbutton.setEnabled(true);
+				print_msg_to_UI("\nNow please set [ Action ], player1\n", MainFrame.textArea);
+				while( blockmain ){
+					// do nothing
+					try {
+					    Thread.sleep(200);                 //1000 milliseconds is one second.
+					} catch(InterruptedException ex) {
+					    Thread.currentThread().interrupt();
+					}
+				}
+				MainFrame.actionbutton.setEnabled(false);
+				// fight
+				blockmain = true;
+				MainFrame.btnFight.setEnabled(true);
+				print_msg_to_UI("\nNow please set [ Fight ], player1\n", MainFrame.textArea);
+				while( blockmain ){
+					// do nothing
+					try {
+					    Thread.sleep(200);                 //1000 milliseconds is one second.
+					} catch(InterruptedException ex) {
+					    Thread.currentThread().interrupt();
+					}
+				}
+				MainFrame.btnFight.setEnabled(false);
+				///// player 2
+				// action
+				turn ++;
+				blockmain = true;
+				MainFrame.actionbutton2.setEnabled(true);
+				print_msg_to_UI("\nNow please set [ Action ], player2\n", MainFrame.textArea);
+				while( blockmain ){
+					// do nothing
+					try {
+					    Thread.sleep(200);                 //1000 milliseconds is one second.
+					} catch(InterruptedException ex) {
+					    Thread.currentThread().interrupt();
+					}
+				}
+				MainFrame.actionbutton2.setEnabled(false);
+				// fight
+				blockmain = true;
+				MainFrame.btnFight2.setEnabled(true);
+				print_msg_to_UI("\nNow please set [ Fight ], player2\n", MainFrame.textArea);
+				while( blockmain ){
+					// do nothing
+					try {
+					    Thread.sleep(200);                 //1000 milliseconds is one second.
+					} catch(InterruptedException ex) {
+					    Thread.currentThread().interrupt();
+					}
+				}
+				MainFrame.btnFight2.setEnabled(false);
+
+				print_msg_to_UI("\nNow Computer counting result and display\n", MainFrame.textArea);
+			}
 		}
 	}
+
+	//////////////////////////////// for terminal usage ///////////////////////////////////////
 
 	// ask player to set offensive and defensive soldier and then count the result of the war. 
 	public static void take_action ( ActivityMenu AM, Player[] player, Tower[] tower ) throws IOException  {
@@ -89,4 +178,34 @@ public class Game{
 			System.out.println();
 		}
 	}
+
+
+	//////////////////////////////// for UI usage ///////////////////////////////////////
+
+	public static void print_status_to_UI( Player[] player, JTextArea textarea){
+		// initialization
+		int i, len;
+
+		// print player status
+		textarea.append(     "________________<Game status>________________\n");
+		for(i=0; i<player.length;i++){
+			textarea.append( "                <player name>     " + player[i].getPlayerName() + "\n");
+			textarea.append( "                <camp name>       " + player[i].getCampName() + "\n");
+			textarea.append( "                <money>           " + player[i].getMoney() + "\n");
+			textarea.append( "                <num of soldier>  " + player[i].getNumSoldier() + "\n");
+			if( i != player.length - 1)
+				textarea.append("\n");
+		}
+		textarea.append(     "_____________________________________________\n");
+
+		len = textarea.getDocument().getLength();
+		textarea.setCaretPosition(len);
+	}
+
+	public static void print_msg_to_UI(String msg, JTextArea textarea){
+		textarea.append(msg);
+		int len = textarea.getDocument().getLength();
+		textarea.setCaretPosition(len);
+	}
+
 }
