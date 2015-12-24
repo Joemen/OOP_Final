7e781c9d041883810934900a53dc4cf9efc00f8a
which0326 <which0326@gmail.com>
2015-12-24 15:14:12 +0800

    add shop constant

--- Constants.java
+++ Constants.java
@@ -9,9 +9,12 @@ public class Constants{
 	// public static final int camp_num = 2;
 	// # camp for a game
 	
-	public static final float advantage_of_def = 1.1f;
+	public static final float advantage_of_def = 1.05f;
 	// # advantage ratio of defense
 
-	public static final int total_round = 3;
+	public static final int total_round = 10;
 	// # total round in a game
+
+	public static final int total_shop_stuff = 1;
+	// # total stuff will be in the shop 
 }
