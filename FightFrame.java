c5af65094916c55b2c3ec2ff449d8ebb103ad71c
which0326 <which0326@gmail.com>
2015-12-27 02:18:38 +0800

    add enter

--- FightFrame.java
+++ FightFrame.java
@@ -39,7 +39,6 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 	private int amount_2 = 0;
 	private NumberFormat amountFormat;
 	private static JButton btnReady;
-	private static JLabel lblIncorrectInput;
 	
 	public static JLabel lblalarm;
 	
@@ -59,10 +58,10 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 		JLabel label_moneyarmyblood = new JLabel(armyblood);
 		label_roundplayer.setForeground(Color.BLUE);
 		label_roundplayer.setBounds(21, 6, 262, 16);
-		fightframe.getContentPane().add(label_roundplayer);
+		fightframe.add(label_roundplayer);
 		label_moneyarmyblood.setForeground(Color.BLUE);
 		label_moneyarmyblood.setBounds(230, 6, 300, 16);
-		fightframe.getContentPane().add(label_moneyarmyblood);
+		fightframe.add(label_moneyarmyblood);
 		
 		
 		JLabel lblSetYourOffense = new JLabel("Set your offense soldier");
@@ -74,39 +73,25 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 		fightframe.getContentPane().add(lblSetYourDefense);
 	
 		
-		amount_Off = new JFormattedTextField(
-			new InternationalFormatter(NumberFormat.getIntegerInstance()) {
-				protected DocumentFilter getDocumentFilter() {
-                return filter;
-            }
-            private DocumentFilter filter = new IntFilter();
-        });
+		amount_Off = new JFormattedTextField(amountFormat);
 		amount_Off.setValue(amount_1);
 		amount_Off.setBounds(53, 109, 96, 21);
 		amount_Off.setColumns(10);
 		amount_Off.addPropertyChangeListener("value", this);
-		
 		fightframe.getContentPane().add(amount_Off);
-
-		amount_Def = new JFormattedTextField(
-			new InternationalFormatter(NumberFormat.getIntegerInstance()) {
-				protected DocumentFilter getDocumentFilter() {
-                return filter;
-            }
-            private DocumentFilter filter = new IntFilter();
-        });
+		
+		amount_Def = new JFormattedTextField(amountFormat);
 		amount_Def.setValue(amount_2);
 		amount_Def.setBounds(272, 109, 96, 21);
 		amount_Def.setColumns(10);
 		amount_Def.addPropertyChangeListener("value", this);
-		
 		fightframe.getContentPane().add(amount_Def);
 		
 		lblalarm = new JLabel("Not enough Soldiers");
 		lblalarm.setForeground(Color.RED);
 		lblalarm.setBounds(400, 107, 291, 27);
 		lblalarm.setVisible(false);
-		fightframe.getContentPane().add(lblalarm);
+		fightframe.add(lblalarm);
 		
 		btnReady = new JButton("Ready");
 		btnReady.setBounds(53, 304, 87, 23);
@@ -115,12 +100,6 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.setBounds(272, 304, 87, 23);
 		fightframe.getContentPane().add(btnCancel);
-		
-		lblIncorrectInput = new JLabel("Incorrect Input!   Please check again!");
-		lblIncorrectInput.setForeground(Color.RED);
-		lblIncorrectInput.setBounds(88, 274, 300, 27);
-		lblIncorrectInput.setVisible(false);
-		fightframe.getContentPane().add(lblIncorrectInput);
 		
 		
 		
@@ -132,28 +111,15 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 
 		btnReady.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int i = -1;
-				int j = -1;
-				try{
-					i = Integer.parseInt(amount_Off.getText());
-					j = Integer.parseInt(amount_Def.getText());
-				} catch (Exception ex){
-					
-				}
-				if( i>=0 && j>=0 && Game.player[(Game.turn+1)%2].getNumSoldier()-((Number)amount_Off.getValue()).intValue()-((Number)amount_Def.getValue()).intValue()>=0){
-					Commander.dispatchArmy_UI(Game.player[(Game.turn+1)%2], Game.tower[Game.player[(Game.turn+1)%2].getCampNum()-1]);
-					Game.blockmain = false;
-					fightframe.dispose();
-				}
-				else if (i<0 || j<0)
-					lblIncorrectInput.setVisible(true);
-				else
-					lblalarm.setVisible(true);
-			}	
+				Commander.dispatchArmy_UI(Game.player[(Game.turn+1)%2], Game.tower[Game.player[(Game.turn+1)%2].getCampNum()-1]);
+				Game.blockmain = false;
+				fightframe.dispose();
+			}
 		});
 		
 
 		fightframe.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		fightframe.getRootPane().setDefaultButton(btnReady);
 	}
 
 	
@@ -168,59 +134,15 @@ public class FightFrame extends JPanel implements PropertyChangeListener  {
 	        } else if (source == amount_Def) {
 	            amount_2 = ((Number)amount_Def.getValue()).intValue();
 	        } 
-	        int i = -1;
-			int j = -1;
-			
-			try{
-				i = Integer.parseInt(amount_Off.getText());
-				j = Integer.parseInt(amount_Def.getText());
-			} catch (Exception ex){
-				
-			}
-			if( i>=0 && j>=0){
-				lblIncorrectInput.setVisible(false);
-			}
 	        
-	        if(Game.player[(Game.turn+1)%2].getNumSoldier()-((Number)amount_Off.getValue()).intValue()-((Number)amount_Def.getValue()).intValue()>=0){	
+	        if(Game.player[(Game.turn+1)%2].getNumSoldier()-Integer.parseInt(amount_Off.getText())-Integer.parseInt(amount_Def.getText())<0){
+				btnReady.setEnabled(false);
+				lblalarm.setVisible(true);
+			}
+			else {
+				btnReady.setEnabled(true);
 				lblalarm.setVisible(false);
 			}
 	}
+	
 }
-
-class IntFilter extends DocumentFilter {
-	//重載insertString方法
-	    public void insertString(FilterBypass fb, int offset, String string,
-	            AttributeSet attr) throws BadLocationException {
-	        StringBuilder builder = new StringBuilder(string);
-	        for (int i = builder.length() - 1; i >= 0; i--) {
-	            int cp = builder.codePointAt(i);
-	            if (!Character.isDigit(cp) && cp != '-') {
-	                builder.deleteCharAt(i);
-	                if (Character.isSupplementaryCodePoint(cp)) {
-	                    i--;
-	                    builder.deleteCharAt(i);
-	                }
-	            }
-	        }
-	        super.insertString(fb, offset, builder.toString(), attr);
-	    }
-	//重載replace方法
-	    public void replace(FilterBypass fb, int offset, int length, String string,
-	            AttributeSet attr) throws BadLocationException {
-	        if (string != null) {
-	            StringBuilder builder = new StringBuilder(string);
-	            for (int i = builder.length() - 1; i >= 0; i--) {
-	                int cp = builder.codePointAt(i);
-	                if (!Character.isDigit(cp) && cp != '-') {
-	                    builder.deleteCharAt(i);
-	                    if (Character.isSupplementaryCodePoint(cp)) {
-	                        i--;
-	                        builder.deleteCharAt(i);
-	                    }
-	                }
-	            }
-	            string = builder.toString();
-	        }
-	        super.replace(fb, offset, length, string, attr);
-	    }
-	}
